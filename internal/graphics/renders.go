8aaf9b82c858a05d9a2c440a66ce73a3da465d64
Atanas Yanev <atanasyanev2@gmail.com>
2023-09-20 08:59:21 +0100

    Setting up the Repo

---
+++ internal/graphics/renders.go
@@ -0,0 +1,14 @@
+package interal
+
+import (
+	"github.com/hajimehoshi/ebiten"
+)
+
+const (
+	screenWidth  = 640
+	screenHeight = 480
+)
+
+func Update(screen *ebiten.Image) error {
+	return nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"github.com/hajimehoshi/ebiten"
+)
+
 func main() {
+	ebiten.SetWindowSize(internal.screenWidth, internal.screenHeight)
+	ebiten.SetWindowTitle("Broforce V2 on Ebiten")
+	if err := ebiten.RunGame(internal.update, internal.screenWidth, internal.screenHeight, 2, "Broforce V2 on Ebiten"); err != nil {
+		panic(err)
+	}
 
 }
