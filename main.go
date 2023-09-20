b44556dffb9c96091b308804ea152f46df5a25cd
Atanas Yanev <atanasyanev2@gmail.com>
2023-09-20 14:57:30 +0100

    Adding basic character

--- main.go
+++ main.go
@@ -1,13 +1,47 @@
 package main
 
 import (
+	"bytes"
+	"image"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"golang.org/x/image/font"
+)
+
+const (
+	screenWidth   = 640
+	screenHeight  = 480
+	tileSize      = 32
+	titleFontSize = fontSize * 1.5
+	fontSize      = 24
+	smallFontSize = fontSize / 2
+)
+
+var (
+	gopherImage     *ebiten.Image
+	tilesImage      *ebiten.Image
+	titleArcadeFont font.Face
+	arcadeFont      font.Face
+	smallArcadeFont font.Face
 )
 
 type Game struct{}
+
+func inint() {
+	img, _, err := image.Decode(bytes.NewReader(images.run_png))
+	if err != nil {
+		log.Fatal(err)
+	}
+	runnerImage := ebiten.NewImageFromImage(img)
+
+	img, _, err := image.Decode(bytes.NewReader(images.idle_png))
+	if err != nil {
+		log.Fatal(err)
+	}
+	idleImage := ebiten.NewImageFromImage(img)
+}
 
 func (g *Game) Update() error {
 	return nil
@@ -22,9 +56,11 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
-	ebiten.SetWindowSize(640, 480)
+	game := &Game{}
+
+	ebiten.SetWindowSize(1000, 650)
 	ebiten.SetWindowTitle("Broforce V2")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
