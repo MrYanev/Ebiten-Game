1f58d9d22a5ec8cfccabecc47eb1e1f846f850fc
Atanas Yanev <atanasyanev2@gmail.com>
2023-09-22 08:18:13 +0100

    Working on Player and Enemies structs

--- internal/game/enemies.go
+++ internal/game/enemies.go
@@ -1,4 +1,11 @@
 package internal
 
+import "github.com/hajimehoshi/ebiten/v2"
+
 type Enemies struct {
+	image      *ebiten.Image
+	xPos, yPos float64
+	speed      float64
 }
+
+var enemieImg *ebiten.Image
--- internal/game/player.go
+++ internal/game/player.go
@@ -1,4 +1,11 @@
 package internal
 
+import "github.com/hajimehoshi/ebiten/v2"
+
 type Player struct {
+	image      *ebiten.Image
+	xPos, yPos float64
+	speed      float64
 }
+
+var playerImg *ebiten.Image
