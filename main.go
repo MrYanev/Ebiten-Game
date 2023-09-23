e76ea81afd7f5436ef32241136eb0f3cfc0d3da0
Atanas Yanev <atanasyanev2@gmail.com>
2023-09-23 08:28:15 +0100

    Fixed issues with rendering

--- main.go
+++ main.go
@@ -7,48 +7,45 @@ import (
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
-type Player struct { //Here for testing purposes
+type Game struct{}
+
+// Our game constants
+const (
+	screenWidth, screenHeight = 640, 480
+)
+
+// Create our empty vars
+var (
+	err        error
+	background *ebiten.Image
+	knight     *ebiten.Image
+	playerOne  player
+)
+
+// Create the player class
+type player struct {
 	image      *ebiten.Image
 	xPos, yPos float64
 	speed      float64
 }
 
-type Game struct{}
-
-const (
-	screenWidth, screenHeight = 1200, 750
-)
-
-var (
-	err        error
-	background *ebiten.Image
-	enemieImg  *ebiten.Image
-	playerImg  *ebiten.Image
-	playerOne  Player
-)
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	background, _, err = ebitenutil.NewImageFromFile("assets/images/background.png", ebiten.FilterDefault)
+// Run this code once at startup
+func init() {
+	background, _, err = ebitenutil.NewImageFromFile("D:\\Code-projects\\Ebiten-Game\\assets\\images\\background.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	playerImg, _, err = ebitenutil.NewImageFromFile("assets/images/_Idle.png", ebiten.FilterDefault)
+	knight, _, err = ebitenutil.NewImageFromFile("D:\\Code-projects\\Ebiten-Game\\assets\\images\\_Idle.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	playerOne = Player{playerImg, screenHeight / 2.0, screenWidth / 2.0, 4} //To be checked
-
-	enemieImg, _, err = ebitenutil.NewImageFromFile("images/_IdleEnem.png", ebiten.FilterDefault)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	playerOne = player{knight, screenWidth / 2.0, screenHeight / 2.0, 4}
 }
 
 func (g *Game) Update(screen *ebiten.Image) error {
-	// Games logic goes here
+	g.MovePlayer()
 	if ebiten.IsDrawingSkipped() {
 		return nil
 	}
@@ -57,17 +54,31 @@ func (g *Game) Update(screen *ebiten.Image) error {
 	screen.DrawImage(background, op)
 
 	playerOp := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(0, 0)
-	screen.DrawImage(playerOne.image, playerOp) //Related to the previous
+	playerOp.GeoM.Translate(playerOne.xPos, playerOne.yPos)
+	screen.DrawImage(playerOne.image, playerOp)
 
 	return nil
+}
+
+func (g *Game) MovePlayer() {
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		playerOne.yPos -= playerOne.speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		playerOne.yPos += playerOne.speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		playerOne.xPos -= playerOne.speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		playerOne.xPos += playerOne.speed
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return 1200, 750
 }
 
-// Main loop
 func main() {
 	game := &Game{}
 	ebiten.SetWindowSize(1200, 750)
